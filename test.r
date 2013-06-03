209ad86470d38786ce27468f3f42814dc5e6c03e
Thomas Levine <occurrence@thomaslevine.com>
2013-06-02 17:15:24 -0700

    real tests

--- test.r
+++ test.r
@@ -1,21 +1,25 @@
-# library(testthat)
+library(testthat)
+source('gastronomify.r')
 
-# The mean recipe should be the base recipe.
-recipe = c(foo = 2, bar = 1, baz = 1)
-observed = gastronomify(
-  x = c(2007, 2007, 2007, 2008, 2008, 2008),
-  y = c(8, 9, 6, 2, 12, 5),
-  group = factor(c('red', 'blue', 'green', 'red', 'blue',  'green')),
-  recipe = recipe
-)
-colSums(observed[-1]) == recipe * 2
+test_that('The mean recipe should be the base recipe.', {
+  recipe = c(foo = 2, bar = 1, baz = 1)
+  observed = gastronomify(
+    x = c(2007, 2007, 2007, 2008, 2008, 2008),
+    y = c(8, 9, 6, 2, 12, 5),
+    group = factor(c('red', 'blue', 'green', 'red', 'blue',  'green')),
+    recipe = recipe
+  )
+  expect_that(as.vector(colSums(observed[-1])), equals(as.vector(recipe * 2)))
+})
 
-# This particular recipe should match.
-expected = data.frame(
-  x = c(2007, 2008),
-  foo = c(1.714286, 2.285714),
-  bar = c(1.0909091, 0.9090909),
-  baz = c(1.6, 0.4)
-)
-colnames(expected)[-1] <- c('foo (blue)', 'bar (green)',  'baz (red)')
-observed == expected
+test_that('This particular recipe should match.', {
+  expected = data.frame(
+    x = c(2007, 2008),
+    foo = c(1.714286, 2.285714),
+    bar = c(1.0909091, 0.9090909),
+    baz = c(1.6, 0.4)
+  )
+  colnames(expected)[-1] <- c('foo (blue)', 'bar (green)',  'baz (red)')
+  expect_that(observed[1], equals(expected[1]))
+  expect_that(round(observed[-1], -5), equals(round(expected[-1], -5)))
+})
