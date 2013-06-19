d4076737e00858f7daf7d2b76a19a3de972d7135
Thomas Levine <occurrence@thomaslevine.com>
2013-06-19 09:17:18 -0700

    oops, annotations don't work like that.

--- R/taskrabbit.r
+++ R/taskrabbit.r
@@ -1,12 +1,12 @@
 #' @import XML
 
-#' Find the authenticity_token in the page.
+# Find the authenticity_token in the page.
 auth.token <- function(text) {
   doc <- htmlParse(text, asText = TRUE)
   xpathApply(doc, '//input[@name="authenticity_token"]', xmlAttrs)[[1]]['value'][[1]]
 }
 
-#' Set up the curl hadle.
+# Set up the curl hadle.
 handle <- function() {
   curl  <- getCurlHandle()
   agent <- 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36'
