15f69ddaaea358f6cdeb21dbe5b8509b030ad8c1
Thomas Levine <occurrence@thomaslevine.com>
2013-10-25 05:48:48 -0700

    lots of examples

--- example.r
+++ example.r
@@ -3,14 +3,28 @@ library(reshape2)
 library(plyr)
 library(gastronomify)
 
-fruit.salad  <-  c(apples = 3, bananas = 1, cherries = 12, grapes = 14,
-                   kiwis = 2, lemons = 0.5, mangos = 1, nectarines = 2, oranges = 2,
-                   pineapples = 0.5, raspberries = 8, watermelons = 0.25)
-
 # Tidy the dataset
 vars <- c('Hair','Eye','Sex')
-ddply(melt(data('HairEyeColor')), vars, function(df) {
+people <- ddply(melt(data('HairEyeColor')), vars, function(df) {
   df[rep(1,df[1,'value']),vars]
 })
 
-ggplot(iris) + aes(meat = Hair, guacamole = Sex) + geom_taco()
+# Plots
+ggplot(hair) + aes(meat = Hair, guacamole = Sex) + geom_taco()
+ggplot(hair) + aes(meat = Eye, cheese = Hair, toasted = Sex) + geom_sandwich()
+
+# x gets mapped to sandwich length,
+# and y gets mapped to the number
+# of pieces of the main filling.
+ggplot(iris) + aes(x = Sepal.Length, y = Sepal.Width, meat = Species)
+
+iris.premelt <- iris
+iris.premelt$id <- rownames(iris)
+iris.molten <- melt(iris.premelt, id.vars = c('Species','id'))
+
+# x gets mapped to the number of items
+# and scaled such that there are no more
+# than four of the same item.
+ggplot(iris.molten) +
+  aes(group = Species, item = Species, x = value) +
+  geom_dimsum()
